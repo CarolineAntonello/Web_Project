8cd41e02e987465e206f070f2c0ed746271d91f8
Caroline Antonello <caroline.antonello@ndd.com.br>
2019-08-15 15:01:43 -0300

    Adicionado classes bases para o contexto

---
+++ Web-Project/Web-Project.Infra.ORM/Context/DbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Infrastructure;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Web_Project.Infra.ORM.Context
+{
+    [ExcludeFromCodeCoverage]
+    public class DbContextFactory : IDbContextFactory<WebProjectContext>
+    {
+        public WebProjectContext Create()
+        {
+            return new WebProjectContext();
+        }
+    }
+}
---
+++ Web-Project/Web-Project.Infra.ORM/Context/WebProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Web_Project.Infra.ORM.Context
+{
+    public class WebProjectContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.AddFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
